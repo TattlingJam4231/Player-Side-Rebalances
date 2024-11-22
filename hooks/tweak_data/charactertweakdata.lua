d505da75e39ff46cdeb47e1436c3bffffa888264
TattlingJam4231 <oryon_1@yahoo.com>
2024-11-21 22:23:16 -0700

    poison hurt applies to medics and dozers

--- hooks/tweak_data/charactertweakdata.lua
+++ hooks/tweak_data/charactertweakdata.lua
@@ -1,5 +1,17 @@
-local _init_fbi_heavy_swat_original = CharacterTweakData._init_fbi_heavy_swat
-function CharacterTweakData:_init_fbi_heavy_swat(presets)
-	_init_fbi_heavy_swat_original(self, presets)
-	self.fbi_heavy_swat.damage.explosion_damage_mul = 1 -- changed from 0.9
-end
+Hooks:PostHook(CharacterTweakData, "_init_fbi_heavy_swat", "Oryo CharacterTweakData _init_fbi_heavy_swat", function(self, presets)
+
+	self.fbi_heavy_swat.damage.explosion_damage_mul = 1 -- oryo: removed explosion resist
+		
+end)
+
+Hooks:PostHook(CharacterTweakData, "_init_medic", "Oryo CharacterTweakData _init_medic", function(self, presets)
+
+	self.medic.damage.hurt_severity = presets.hurt_severities.only_poison_and_light_hurt -- oryo: allow poison hurt on medic
+		
+end)
+
+Hooks:PostHook(CharacterTweakData, "_init_tank", "Oryo CharacterTweakData _init_tank", function(self, presets)
+
+	self.tank.damage.hurt_severity = presets.hurt_severities.only_poison_and_light_hurt -- oryo: allow poison hurt on dozer
+		
+end)
